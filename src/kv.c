b8be89487eee3e15a63417a6596296c1d4c49fcc
Wild-Things <sharpeproduction@gmail.com>
2026-03-12 16:36:44 -0400

    Added database put functionality

--- src/kv.c
+++ src/kv.c
@@ -1,4 +1,69 @@
 #include <kv.h>
+#include <string.h>
+#include <stdlib.h>
+
+#define TOMBSTONE 0x1
+
+size_t hash(char *val, int capacity) {
+	size_t hash = 0x8021180211802118;
+
+	while(*val) {
+		hash ^= *val;
+		hash = hash << 8;
+		hash += *val;
+
+		val++;
+	}
+
+	return hash % capacity;
+}
+
+// fn kv_put
+// params:
+//  - db: a pointer to the database memory address
+//  - key: a pointer to the key memory address
+//  - value: a pointer to the value memory address
+// Returns the index of the key, otherwise on error
+// return -1, on not found returns -2
+
+int kv_put(kv_t *db, char *key, char *value) {
+	if (!db || !key || !value) return -1;
+
+	size_t idx = hash(key, db->capacity);
+
+	for (int i = 0; i < db->capacity-1; i++) {
+		
+		size_t real_idx = (idx + 1) % db->capacity;
+
+		kv_entry_t *entry = &db->entries[real_idx];
+
+		if (entry->key &&
+		    entry->key != (void*)TOMBSTONE &&
+		    !strcmp(entry->key, key)) {
+			char *newval = strdup(value);
+			if (!newval) return -1;
+			entry->value = newval;
+			return real_idx;
+		}
+
+		if (!entry->key || entry->key == (void*)TOMBSTONE) {
+			char *newval = strdup(value);
+			char *newkey = strdup(key);
+			if (!newval || !newkey) { 
+				free(newkey);
+				free(newval);
+				return -1;
+			}
+			entry->value = newval;
+			entry->key = newkey;
+			db->count++;
+			return real_idx;
+		}	
+	}
+	// if we get to the end, the database is occupied and
+	// we return -2
+	return -2;
+}
 
 kv_t *kv_init(size_t capacity) {
 	if (capacity == 0) return NULL;
